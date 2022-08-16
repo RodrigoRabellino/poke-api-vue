aa8cd2a13f1e9535df336e6dc0c6730d96342671
Rodrigo Rabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2022-08-16 15:51:16 -0300

    styles

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import PokemonView from "./PokemonView.vue";
+import pokeball from "@/assets/pokeball.svg";
+import pokeballOpen from "@/assets/pokeball-open.svg";
 import { usePokeStore } from "../store/store";
 
 const store = usePokeStore();
@@ -32,12 +34,19 @@ onMounted(() => {
       </div>
 
       <div
-        href="#"
         class="item-list"
         v-for="pokemon in store.pokemonFilter"
         :key="pokemon.entry_number"
         @click.prevent="handleClick(pokemon.entry_number)"
       >
+        <img
+          :src="
+            pokemon.entry_number === store.pokemonCurrent.id
+              ? pokeballOpen
+              : pokeball
+          "
+          alt=""
+        />
         <p>{{ pokemon.entry_number }}-</p>
         <p>{{ pokemon.pokemon_species.name }}</p>
       </div>
@@ -83,12 +92,18 @@ onMounted(() => {
   outline: none;
 }
 .item-list {
+  align-items: center;
   display: flex;
   cursor: pointer;
   transition: 0.4s;
   background-color: #efefef;
   padding: 1rem;
 }
+.item-list img {
+  width: 24px;
+  margin-right: 10px;
+}
+
 .item-list:hover {
   transition: 0.4s;
   background-color: white;
